b19b5f41d5e06773c04bff789c8fcaa61f651dca
Winston-Tam <winstontam28@gmail.com>
2025-05-06 16:23:03 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -10,12 +10,13 @@ for (String singleWord : wordArray)
 words.add(singleWord);
 }
 }
-{
-public String getNext()
+
+public String getNext(){
 if (words.size() > 0)
 {
 return words.remove((int)(Math.random() * words.size()));
 }
 return "NONE";
 }
+  
 }
